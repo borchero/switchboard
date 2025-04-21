5b3f420662395c085bcfc5197c057e190870a78f
Oliver Borchert <oliver.borchert@quantco.com>
2025-04-21 18:16:04 +0200

    Try

--- internal/switchboard/targets_test.go
+++ internal/switchboard/targets_test.go
@@ -37,7 +37,7 @@ func TestServiceTargetTargets(t *testing.T) {
 	require.Nil(t, err)
 
 	// Check whether we find the load balancer IP
-	time.Sleep(time.Second)
+	time.Sleep(5 * time.Second)
 	name := client.ObjectKeyFromObject(&service)
 	err = ctrlClient.Get(ctx, name, &service)
 	require.Nil(t, err)
